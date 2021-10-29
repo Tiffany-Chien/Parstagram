5cdaf47445459143a6e98f10cc1aa09458a158e2
Tiffany <tiffanychien9@gmail.com>
2021-10-29 04:59:50 -0400

    allow user stays logged in across restarts

--- Instagram/SceneDelegate.swift
+++ Instagram/SceneDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Parse
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
@@ -17,6 +18,16 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let _ = (scene as? UIWindowScene) else { return }
+        
+        // check if the user is logged in
+        if PFUser.current() != nil {
+            let main = UIStoryboard(name:"Main", bundle: nil)
+            let feedNavigationController = main.instantiateViewController(withIdentifier: "FeedNavigationColtroller")
+            // stop and restart would show the feed controller instead of
+            // login screen
+            window?.rootViewController = feedNavigationController
+        }
+        
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
